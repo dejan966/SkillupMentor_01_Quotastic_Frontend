a9919beebed9603cc80ba84f9fe8c3c4f8e25691
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-02-05 19:59:34 +0100

    Footer, sidebar and layout done

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => {
   return (
     <footer className="container-xxl d-flex justify-content-between align-items-center p-4">
       <img src="/images/logo.png" alt="SkillUp Mentor" width={123} />
-      <p className="mb-0">&copy; 2023</p>
+      <p className="fs-4">All rights received | skillupmentor.com</p>
     </footer>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,22 +6,11 @@ const Home: FC = () => {
     <Layout>
       <div className="p-2 mb-4">
         <div className="container-fluid py-4">
-          <h1 className="display-5 fw-bold">Welcome</h1>
+          <h1 className="display-5">Welcome to {/*Span*/} Quotastic</h1>
           <p className="col-md-8 fs-4">
-            In this tutorial you will learn how to use:
+            Quotastic is a free online tool for you to explore the quips, quotes and proverbs.
+            Sign up and express yourself. 
           </p>
-          <ol>
-            <li>react-bootstrap</li>
-            <li>react-hook-form with yup validation</li>
-            <li>CRUD functionality - Create Read Update Delete</li>
-            <li>file upload with NestJS</li>
-            <li>connect ReactJS and NestJS</li>
-            <li>setup NestJS with custom logging</li>
-            <li>connect to PostgreSQL database using TypeORM</li>
-            <li>create dtos with validation</li>
-            <li>deploy ReactJS to AWS S3 Bucket</li>
-            <li>deploy NestJS to AWS ECS (Elastic Container Service)</li>
-          </ol>
         </div>
       </div>
     </Layout>
